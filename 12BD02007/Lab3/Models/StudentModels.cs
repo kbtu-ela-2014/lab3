e8ab914853daa035842df45a4fc2e7205a9c0356
unknown <sergey.gordeev94@gmail.com>
2014-10-13 15:40:14 +0600

    third lab

---
+++ 12BD02007/Lab3/Models/StudentModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lab3;
+using System.Data.Linq;
+
+namespace Lab3.Models
+{
+    #region Models
+    public partial class Student
+    {
+        public Student(int id, string name) {
+            this.ID = id;
+            this.Name = name;
+        }
+
+        public Student(string name)
+        {
+            this.Name = name;
+        }
+
+        public Student(string name, decimal GPA)
+        {
+            this.Name = name;
+            this.GPA = GPA;
+        }
+
+        public Student(int id, string name, decimal GPA)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.GPA = GPA;
+        }
+
+        //public int ID {get; set;}
+        //public string Name {get; set;}
+    }
+    #endregion
+}
---
+++ 12BD02007/Lab3/Views/Scripts/Main.js
@@ -0,0 +1,31 @@
+﻿var counter = 0;
+var ActiveId = null;
+
+function edit(id) {
+    if (count != 0) {
+        if (ActiveId == id) {
+            save();
+        }
+
+        alert("Previous changes should be submited before making new ones!")
+        return;
+    }
+
+    var btn = document.getElementById(id);
+    var field = btn.parentNode.nextSibling.nextSibling;
+
+    while (field != null) {
+        field.innerHTML = '<input type="text" name="Edited" value="'+ field.innerHTML.trim() +'"/>';
+        field = field.nextSibling;
+    }
+
+    btn.value = "Save";
+    ActiveId = id;
+
+    counter = 1;
+}
+
+function save() {
+    document.forms[0].action = "Student/Edit";
+    document.forms[0].submit();
+}
